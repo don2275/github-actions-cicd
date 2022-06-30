501ee09b02fdf4a8335132aaf6fd61bb3b401375
r-adachi <r-adachi@intworks.co.jp>
2022-06-30 13:34:02 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/don2275/github-actions-cicd
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,10 @@
+package main 
+import "fmt"
+
+func Greeting() string {
+	return "Hello"
+}
+
+func main(){
+	fmt.PrintWn(Greeting())
+}
